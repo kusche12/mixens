d2f5e26d69f1f479a8e0d240a8f63a8a6aa8eca7
kyle <funnyguykyo@gmail.com>
2020-07-06 08:45:07 -0700

    Use React Nav stack navigator for Create Mix Header

--- App.js
+++ App.js
@@ -79,7 +79,7 @@ authFlow.navigationOptions = () => {
       if (focused) {
           iconColor = '#64CAF6';
       }
-      return <FontAwesome5 name="user-alt" size={30} color={iconColor} style={{ top: 1 }} />;
+      return <FontAwesome5 name="user-alt" size={28} color={iconColor} style={{ top: 1 }} />;
     },
     tabBarOptions: {
       activeTintColor: '#64CAF6',
@@ -88,9 +88,14 @@ authFlow.navigationOptions = () => {
   }
 }
 
+const createFlow = createStackNavigator({
+  Create: CreateScreen,
+  detailFlow
+});
+
 const Navigator = createBottomTabNavigator({
   detailFlow,
-  Create: CreateScreen,
+  createFlow,
   searchFlow,
   authFlow
 });
@@ -109,10 +114,11 @@ export default function App() {
 };
 
 // TODO STYLE:
-// Style the navbar: https://reactnavigation.org/docs/headers/
+// Style the navbar: !! https://reactnavigation.org/docs/headers/ !!
 
 // TODO DEV:
 // Navigate from Tag in DetailScreen to specific query of all drinks with that tag
+// BUG The first ingredient text and tag text inputs are not rendered in Create Mix
 
 //    Currently, when the user submits a new drink, the navigation does not recognize this as a 
 //    blur effect. This means that it will never understand onDidFocus again after 1 creation
